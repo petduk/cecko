1fde203ca9c4d912f2558414a1d16b14fff55559
Petr <petr@epsilon.localdomain>
2018-01-18 19:40:49 +0100

    str.29

---
+++ cv_1.3.5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* vypíše tabulku Fahrenheit-Celsius 
+
+error: expected ‘;’ before ‘)’ token
+     for(fahr = 0; fahr <= 300, fahr = fahr + 20)
+*/
+
+int main()
+
+{
+    int fahr;
+    
+    for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf("%3d %6.1f \n", fahr, (5.0/9.0)*(fahr-32));
+}
